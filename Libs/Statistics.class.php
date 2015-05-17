5174af708977c3c4b8a19cc0a72af0225658c230
Mark Roszko <mark.roszko@gmail.com>
2015-05-16 23:54:19 -0400

    Fix comment block indentation

--- Libs/Statistics.class.php
+++ Libs/Statistics.class.php
@@ -13,20 +13,20 @@
   */
 class Statistics
 {
-/**
- * modShipResists method
- * The bonus' for the ship and creates rules for them
- *
- * @param $_arr (array)
- * @param $_shieldResistPos (int)
- * @param $_section (string)
- * @param $_modName (string)
- * @param $_resist (string)
- * @param $_bonus (int)
- * @param $_type (string)
- * @param $_order (int)
- * @return (array)
- */
+	/**
+	 * modShipResists method
+	 * The bonus' for the ship and creates rules for them
+	 *
+	 * @param $_arr (array)
+	 * @param $_shieldResistPos (int)
+	 * @param $_section (string)
+	 * @param $_modName (string)
+	 * @param $_resist (string)
+	 * @param $_bonus (int)
+	 * @param $_type (string)
+	 * @param $_order (int)
+	 * @return (array)
+	 */
 	public static function modShipResists($_arr, &$_shieldResistPos, $_section, $_modName, $_resist, $_bonus, $_type, $_order) {
 		$_arr[$_shieldResistPos]['name']	= $_modName;
 		$_arr[$_shieldResistPos]['section']	= $_section;
@@ -40,19 +40,19 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modShipEnergy method
- * The modification of the energy type used within a ships mods
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_eng (int)
- * @param $_name (int)
- * @param $_amount (int)
- * @param $_type (int)
- * @param $_mode (int)
- * @return (array)
- */
+	/**
+	 * modShipEnergy method
+	 * The modification of the energy type used within a ships mods
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_eng (int)
+	 * @param $_name (int)
+	 * @param $_amount (int)
+	 * @param $_type (int)
+	 * @param $_mode (int)
+	 * @return (array)
+	 */
 	public static function modShipEnergy($_arr, $_moduleCount, $_eng, $_name, $_amount, $_type, $_mode, $_stack = -1) {
 		$_arr[$_moduleCount]['name'] = $_name;
 		$_arr[$_moduleCount][$_eng]	= $_amount;
@@ -64,19 +64,19 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modScanning method
- * The modification of the energy type used within a ships mods
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_eng (int)
- * @param $_name (int)
- * @param $_amount (int)
- * @param $_type (int)
- * @param $_mode (int)
- * @return (array)
- */
+	/**
+	 * modScanning method
+	 * The modification of the energy type used within a ships mods
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_eng (int)
+	 * @param $_name (int)
+	 * @param $_amount (int)
+	 * @param $_type (int)
+	 * @param $_mode (int)
+	 * @return (array)
+	 */
 	public static function modScanning($_arr, $_moduleCount, $_bonusType, $_bonus, $_neg, &$_negCount, $_type, $_order) {
 		$_arr[$_moduleCount][$_bonusType]	= $_bonus;
 		$_arr[$_moduleCount][$_neg]			= $_negCount;
@@ -87,16 +87,16 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modOrdering method
- * The modification of the scanning equipment
- *
- * @param $_arr (array)
- * @param $_sensorbooster (array)
- * @param $_scan (bool)
- * @param $_range (bool)
- * @return (array)
- */
+	/**
+	 * modOrdering method
+	 * The modification of the scanning equipment
+	 *
+	 * @param $_arr (array)
+	 * @param $_sensorbooster (array)
+	 * @param $_scan (bool)
+	 * @param $_range (bool)
+	 * @return (array)
+	 */
 	public static function modOrdering($_arr, &$_sensorbooster, $_scan, $_range) {
 		$_arr[$_sensorbooster[0]]['scan'] = ($_scan)?$_arr[$_sensorbooster[0]]['range']*2:0;
 		$_arr[$_sensorbooster[0]]['range'] = ($_range)?$_arr[$_sensorbooster[0]]['range']*2:0;
@@ -107,17 +107,17 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modShieldDur method
- * The modification of the shield duration
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_bonus (int)
- * @param $_type (int)
- * @param $_neg (int)
- * @return (array)
- */
+	/**
+	 * modShieldDur method
+	 * The modification of the shield duration
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_bonus (int)
+	 * @param $_type (int)
+	 * @param $_neg (int)
+	 * @return (array)
+	 */
 	public static function modShieldDur($_arr, $_moduleCount, $_bonus, $_type, &$_neg, $inc_count = false) {
 		$_arr[$_moduleCount]['dur'] = $_bonus;
 		$_arr[$_moduleCount]['type'] = $_type;
@@ -128,17 +128,17 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modDuration method
- * The modification of the cap duration
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_indname (int)
- * @param $_indval (int)
- * @param $_duration (int)
- * @return (array)
- */
+	/**
+	 * modDuration method
+	 * The modification of the cap duration
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_indname (int)
+	 * @param $_indval (int)
+	 * @param $_duration (int)
+	 * @return (array)
+	 */
 	public static function modDuration($_arr, $_moduleCount, $_indname, $_indval, $_duration) {
 		$_arr[$_moduleCount][$_indname] = $_indval;
 		$_arr[$_moduleCount]['duration'] = $_duration;
@@ -146,17 +146,17 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modDuration method
- * The modification of the cap duration
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_indname (int)
- * @param $_indval (int)
- * @param $_duration (int)
- * @return (array)
- */
+	/**
+	 * modDuration method
+	 * The modification of the cap duration
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_indname (int)
+	 * @param $_indval (int)
+	 * @param $_duration (int)
+	 * @return (array)
+	 */
 	public static function modCapneed($_arr, $_moduleCount, $_modName, $_capacity, $_capNeeded) {
 		if($_modName != null) $_arr[$_moduleCount]['name'] = $_modName;
 		if($_capacity != null) $_arr[$_moduleCount]['capacity'] = $_capacity;
@@ -165,35 +165,35 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modDurationBoost method
- * The modification of the boost duration
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_duration (int)
- * @return (array)
- */
+	/**
+	 * modDurationBoost method
+	 * The modification of the boost duration
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_duration (int)
+	 * @return (array)
+	 */
 	public static function modDurationBoost($_arr, $_moduleCount, $_duration) {
 		$_arr[$_moduleCount]['dur'] = $_duration;
 		return $_arr;
 	}
 
-/**
- * modSpeed method
- * The modification of the speed of guns
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_gunPos (array)
- * @param $_gunPosCap (array)
- * @param $_gunRoF (string)
- * @param $_amount (int)
- * @param $_type (string)
- * @param $_capacity (int)
- * @param $_techlevel (int)
- * @return (array)
- */
+	/**
+	 * modSpeed method
+	 * The modification of the speed of guns
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_gunPos (array)
+	 * @param $_gunPosCap (array)
+	 * @param $_gunRoF (string)
+	 * @param $_amount (int)
+	 * @param $_type (string)
+	 * @param $_capacity (int)
+	 * @param $_techlevel (int)
+	 * @return (array)
+	 */
 	public static function modSpeed($_arr, $_moduleCount, &$_gunPos, &$_gunPosCap, $_name, $_gunRoF, $_amount, $_type, $_capacity, $_techlevel) {
 		$_gunPos[] 		= $_moduleCount;
 		$_gunPosCap[] 	= $_moduleCount;
@@ -207,36 +207,35 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modDamage method
- * The modification of the damage of guns
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_gunDam (string)
- * @param $_amount (int)
- * @return (array)
- */
+	/**
+	 * modDamage method
+	 * The modification of the damage of guns
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_gunDam (string)
+	 * @param $_amount (int)
+	 * @return (array)
+	 */
 	public static function modDamage($_arr, $_moduleCount, $_gunDam, $_amount) {
 		$_arr[$_moduleCount][$_gunDam] = $_amount;
 
 		return $_arr;
 	}
 
-
-/**
- * modDamageMods method
- * The modification of the damage of damage mods
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_modName (string)
- * @param $_neg (int)
- * @param $_damageType (string)
- * @param $_damage (int)
- * @param $_rofneg (int)
- * @return (array)
- */
+	/**
+	 * modDamageMods method
+	 * The modification of the damage of damage mods
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_modName (string)
+	 * @param $_neg (int)
+	 * @param $_damageType (string)
+	 * @param $_damage (int)
+	 * @param $_rofneg (int)
+	 * @return (array)
+	 */
 	public static function modDamageMods($_arr, $_moduleCount, $_modName, &$_neg, $_damageType, $_damage, &$_rofneg = null) {
 		$_arr[$_moduleCount]['name'] = $_modName;
 		$_arr[$_moduleCount]['neg'] = $_neg++;
@@ -247,20 +246,19 @@ class Statistics
 		return $_arr;
 	}
 
-
-/**
- * modDroneDamageMods method
- * The modification of the damage of damage mods
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_modName (string)
- * @param $_neg (int)
- * @param $_rofdr (int)
- * @param $_damagedr (int)
- * @param $_type (int)
- * @return (array)
- */
+	/**
+	 * modDroneDamageMods method
+	 * The modification of the damage of damage mods
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_modName (string)
+	 * @param $_neg (int)
+	 * @param $_rofdr (int)
+	 * @param $_damagedr (int)
+	 * @param $_type (int)
+	 * @return (array)
+	 */
 	public static function modDroneDamageMods($_arr, $_moduleCount, $_modName, &$_neg, $_rofdr, $_damagedr, $_type) {
 		$_arr[$_moduleCount]['name'] = $_modName;
 		$_arr[$_moduleCount]['neg'] = $_neg;
@@ -273,17 +271,17 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modTankBoost method
- * The modification of the ships tank boost
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_boost (int)
- * @param $_type (string)
- * @param $_neg (int)
- * @return (array)
- */
+	/**
+	 * modTankBoost method
+	 * The modification of the ships tank boost
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_boost (int)
+	 * @param $_type (string)
+	 * @param $_neg (int)
+	 * @return (array)
+	 */
 	public static function modTankBoost($_arr, $_moduleCount, $_boost, $_type, &$_neg = null) {
 		$_arr[$_moduleCount]['boost'] = $_boost;
 		$_arr[$_moduleCount]['type'] = $_type;
@@ -296,17 +294,17 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * modShipeffects method
- * The modification of the ships tank boost
- *
- * @param $_arr (array)
- * @param $_moduleCount (int)
- * @param $_boost (int)
- * @param $_type (string)
- * @param $_neg (int)
- * @return (array)
- */
+	/**
+	 * modShipeffects method
+	 * The modification of the ships tank boost
+	 *
+	 * @param $_arr (array)
+	 * @param $_moduleCount (int)
+	 * @param $_boost (int)
+	 * @param $_type (string)
+	 * @param $_neg (int)
+	 * @return (array)
+	 */
 	public static function modShipeffects($_arr, &$_moduleCount, $_effect, $_bonus, $_type) {
 		$_moduleCount++;
 		$_arr[$_moduleCount]['effect'] = $_effect;
@@ -316,13 +314,13 @@ class Statistics
 		return $_arr;
 	}
 
-/**
- * slots method
- * Converts the new slot ints to the old style
- *
- * @param $slot (int)
- * @return (int)
- */
+	/**
+	 * slots method
+	 * Converts the new slot ints to the old style
+	 *
+	 * @param $slot (int)
+	 * @return (int)
+	 */
 	public static function slots($_slot, $_name, $_cat) {
 		if($_slot == 0) {
 			switch($_name) {
